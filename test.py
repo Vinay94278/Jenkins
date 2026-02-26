e1f0460206a1f27c2d8c4e5ddc20afe19ab9469b
Vinay Aadatiya <adatiyavinayshaileshbhai@gmail.com>
2026-02-27 00:45:06 +0530

    feat: new line added for Github Polling

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print("Hello World !!!")
 print("This is first which i am going to run it on jenkins ")
+print("checking how it automatically poll and re build entire project or run a particular script.")
